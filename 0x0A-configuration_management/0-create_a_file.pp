4bca02ef2246d3e9160bceb5e9e5a8fd3221cd5b
Dc-cpu-arch <d.garzong77@gmail.com>
2021-04-12 20:48:30 -0500

    Syntax Fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@
 file { 'holberton':
   ensure  => file,
   path    => '/tmp/holberton',
-  mode    => ''744',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
